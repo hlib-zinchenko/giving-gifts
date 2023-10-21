6dd740dbf5a940ef946b017fa9142c94683842b6
hlib-zinchenko <hlib.zinchenko@gmail.com>
2023-10-21 12:48:25 +0300

    rearrange solution folders, usings clean up

--- src/GivingGifts.WebAPI/WebApplicationBuilderExtensions.cs
+++ src/GivingGifts.WebAPI/WebApplicationBuilderExtensions.cs
@@ -1,8 +1,6 @@
 using System.Text;
 using GivingGifts.SharedKernel.Core;
-using GivingGifts.SharedKernel.Infrastructure;
 using GivingGifts.Users.Infrastructure;
-using GivingGifts.Users.Infrastructure.Data;
 using GivingGifts.Users.Infrastructure.JWT;
 using GivingGifts.Users.UseCases;
 using GivingGifts.WebAPI.Auth;
