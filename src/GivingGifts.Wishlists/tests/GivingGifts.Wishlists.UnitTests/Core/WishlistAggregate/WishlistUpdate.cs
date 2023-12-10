10b7989f0c2c0abbb0b87d4836864288216e510b
hlib-zinchenko <hlib.zinchenko@gmail.com>
2023-12-10 17:05:53 +0200

    Update WishlistUpdate tests for datetime dependency
    Added IDateTimeProvider to WishlistUpdate test to handle datetime-related operations within the test workflow. The IDateTimeProvider is mocked with NSubstitute and is inserted as a new parameter in the _sut.Update method calls. This makes the method calls independent of the actual system time.

--- src/GivingGifts.Wishlists/tests/GivingGifts.Wishlists.UnitTests/Core/WishlistAggregate/WishlistUpdate.cs
+++ src/GivingGifts.Wishlists/tests/GivingGifts.Wishlists.UnitTests/Core/WishlistAggregate/WishlistUpdate.cs
@@ -1,4 +1,6 @@
+using GivingGifts.SharedKernel.Core;
 using GivingGifts.Wishlists.Core.WishlistAggregate;
+using NSubstitute;
 using Xunit;
 
 namespace GivingGifts.Wishlists.UnitTests.Core.WishlistAggregate;
@@ -6,12 +8,17 @@ namespace GivingGifts.Wishlists.UnitTests.Core.WishlistAggregate;
 public class WishlistUpdate
 {
     private readonly Wishlist _sut = TestData.CreateWishlist();
+    private readonly IDateTimeProvider _dateTimeProvider = Substitute.For<IDateTimeProvider>();
 
+    public WishlistUpdate()
+    {
+        _dateTimeProvider.UtcNow.Returns(DateTime.UtcNow);
+    }
     [Fact]
     public void UpdateSuccess()
     {
         const string newName = "NewTestName";
-        _sut.Update(newName);
+        _sut.Update(newName, _dateTimeProvider);
 
         Assert.Equal(newName, _sut.Name);
     }
@@ -19,6 +26,6 @@ public class WishlistUpdate
     [Fact]
     public void ThrowExceptionWhenNullNamePassed()
     {
-        Assert.Throws<ArgumentNullException>(() => { _sut.Update(null!); });
+        Assert.Throws<ArgumentNullException>(() => { _sut.Update(null!, _dateTimeProvider); });
     }
 }
